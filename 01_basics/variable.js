3913d2dc7ac71fed69e5c8f2b37cf98e57230f77
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-23 17:15:26 +0000

    learn about variable in js

---
+++ 01_basics/variable.js
@@ -0,0 +1,25 @@
+const accountId = 14453
+let accountEmail = "agrawalharsh2653@gmail.com"
+var accountPassword = "h@rsh"
+accountCity = "Bharatpur"
+let accountState;
+
+// accountId = 2 // const variable are not allow to change
+
+accountEmail="harsh543@gmail.com"
+accountPassword = "h@rsh#"
+accountCity = "Jaipur"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
