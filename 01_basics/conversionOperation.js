d5f35ce1c3d6f6ec0c0f5f38392b89e5cb4112df
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-24 19:49:18 +0000

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber)) //number
+console.log(valueInNumber) //1
+
+// To check the change the value of score
+// "44" => 33
+// "44abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+
+let isLoggedIn = 23
+let valueInBoolean = Boolean(isLoggedIn)
+console.log(valueInBoolean); // true
+
+// 1 => true; 0 => false; 11 => true or any nuber change in true
+// "" => false; "harsh" => true
+// null =>0
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber) /"44"
+console.log(typeof(stringNumber)) //string
