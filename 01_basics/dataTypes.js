2dc0c5c878a5041544ad87edeb06e41e64f7b454
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-24 19:23:52 +0000

    learning dataTypes in js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3+5)
+console.log("Harsh")
+
+let name = "Harsh"
+let age = 20
+let isLoggedId = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+//string = ""'
+// boolean => true/false;
+// null = standard value
+// undefined => 
+// symbol => unique
+
+//Object
+console.log(typeof name) //string
+console.log(typeof null) //object
+
