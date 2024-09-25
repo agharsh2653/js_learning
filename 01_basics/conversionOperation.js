202375e8c90fd8bd8c4b56f6623464fd16bfccdd
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-25 17:13:43 +0000

    learning operations in js

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -4,8 +4,8 @@ let score = true
 //console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber)) //number
-console.log(valueInNumber) //1
+// console.log(typeof(valueInNumber)) //number
+// console.log(valueInNumber) //1
 
 // To check the change the value of score
 // "44" => 33
@@ -16,7 +16,7 @@ console.log(valueInNumber) //1
 
 let isLoggedIn = 23
 let valueInBoolean = Boolean(isLoggedIn)
-console.log(valueInBoolean); // true
+//console.log(valueInBoolean); // true
 
 // 1 => true; 0 => false; 11 => true or any nuber change in true
 // "" => false; "harsh" => true
@@ -24,5 +24,34 @@ console.log(valueInBoolean); // true
 
 let someNumber = 44
 let stringNumber = String(someNumber)
-console.log(stringNumber) /"44"
-console.log(typeof(stringNumber)) //string
+// console.log(stringNumber) /"44"
+// console.log(typeof(stringNumber)) //string
+
+// *****************************Operations****************************************
+let value = 3
+let negvalue = -value
+//console.log(value+negvalue)
+
+let str1 = "hello"
+let str2 = " world!"
+let str3 = str1+str2
+//console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2"+2)
+
+//console.log(2+3*4%10) this is called a mexi code company level do not write this type of code
+
+console.log(+true)
+//console.log(true+) it will give error
+console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
