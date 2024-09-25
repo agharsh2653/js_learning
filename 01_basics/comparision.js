8fc4d33ad9290ce8b5f289af247f6a7e96ae8020
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-25 17:29:12 +0000

    comparision in js

---
+++ 01_basics/comparision.js
@@ -0,0 +1,26 @@
+/*
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(8==2**3);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+ 
+//Avoid this type of confusion because it takes in the phage of conflict decision
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+*/
+
+//=== It will stricktly check the value of data type is same or another conditions are fellows
+
+console.log("2"===2)
