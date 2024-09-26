6cf25eec8a7d3877279bc95e6ee3616bd6e4ab06
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-26 18:23:07 +0000

    basic concept of number in math

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+/*
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+*/
+
+const otherNumber = 121.8944
+//console.log(otherNumber.toPrecision(4));
+
+const Numbers = 1000000
+//console.log(Numbers.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++Maths++++++++++++++++++++++++++
+
+/*
+console.log(Math)
+console.log(Math.abs(-7.5))
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.8))
+console.log(Math.min(4, 6,32,90,21))
+console.log(Math.max(45, 32, 89, 21))
+*/
+console.log(Math.random())
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10+1))
+
+const min = 21;
+const max =  45;
+console.log(Math.floor(Math.random() * (max-min +1))+min)
