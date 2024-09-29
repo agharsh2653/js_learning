acd43503104157a065074e4eb738f2ea022629af
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-09-29 18:15:28 +0000

    objects basics in js

---
+++ 02_basics/objects2.js
@@ -0,0 +1,52 @@
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+        userfullname: "harsh",
+        lastname: "agrawal"
+    }
+}
+
+//console.log(regularUser.fullname.userfullname)
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"a", 2:"b"}
+
+//const obj3 = {obj1, obj2} //not right way to join
+//const obj3 = Object.assign({},obj1,obj2,obj4)
+// assign have two parameter target, source
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3)
+
+const users = [
+    {
+        email:"agharsh@gmail.com",
+        password:"123456y7"
+    },
+    {
+        email:"agharsh@gmail.com",
+        password:"123456y7"
+    },
+    {
+        email:"agharsh@gmail.com",
+        password:"123456y7"
+    }
+]
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
