1b610cf27687bc42107426c40c03b4d8b2a9a396
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-01 17:49:04 +0000

    Scope level and mini hosting in js

--- 02_basics/scopes.js
+++ 02_basics/scopes.js
@@ -7,6 +7,44 @@ if(true){
     console.log(`Inner : ${a} <-- Block Scope`)
 }
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c) //30 thats by we are not use var in js
+
+function one(){
+    const username ="hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+
+one()
+
+if(true){
+    const username = "hitesh"
+    if(username == "hitesh"){
+        const website = " youtube"
+        console.log(username+website);
+    }
+    //console.log(website)
+}
+
+//console.log(username)
+
+//  ++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++
+//Hosting concept
+console.log(addone(5))
+//Function
+function addone(num){
+    return num+1;
+}
+
+//addTwo(6) hosting concept
+//Expresion
+const addTwo = function(num){
+    return num +2
+}
