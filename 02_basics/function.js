fcae60c9450a78a637ad83596cf25fd04e6d20c0
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-01 17:18:24 +0000

    function with objects and array in js

--- 02_basics/function.js
+++ 02_basics/function.js
@@ -13,7 +13,7 @@ function addTwoNum(num1,num2){
     return num1+num2;
 }
 
-console.log(addTwoNum(5,70));
+//console.log(addTwoNum(5,70));
 
 function loginUserMessage(username = "hitesh"){
     if(!username){
@@ -23,4 +23,34 @@ function loginUserMessage(username = "hitesh"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("harsh"))
+//console.log(loginUserMessage("harsh"))
+
+function calculateCartPrice(var1,var2,...num1){
+    //rest operator --> ...num1
+    return num1;
+}
+console.log(calculateCartPrice(100,245,567,909,719));
+
+const user ={
+    Username: "agharsh53",
+    price: 999
+
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.Username} and price is ${anyObject.price}`)
+}
+
+//handleObject(user);
+
+handleObject({
+    Username: "hitesh",
+    price: 890
+})
+
+const myNewArray = [200,300,400,500];
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray))
