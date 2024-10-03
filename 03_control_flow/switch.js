2807bfff04635f5040d276b18296c2353cf73916
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-03 12:37:14 +0000

    switch basics in js

---
+++ 03_control_flow/switch.js
@@ -0,0 +1,31 @@
+//Switch syntax
+/*
+switch(key){
+    case value:
+        break;
+
+    default:
+        break;
+}
+*/
+const month =3
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febrary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Default case match");
+        break;
+}
