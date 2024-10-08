f34a052a2cf1d27fcb0a0405ce9d3bcde06bd625
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-08 15:15:14 +0000

    while or do while loop in js

---
+++ 04_iteration/while_loop.js
@@ -0,0 +1,23 @@
+ 
+ // while loop
+
+ let index = 0
+ while(index<=10){
+    console.log(`Value of index is ${index}`)
+    index+=2;
+ }
+
+ let myArray = ['Iron man', "Captain America", "Spider man"]
+ let arr=0
+ while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr+=1
+ }
+
+ // do while loop
+
+ let score =1
+ do {
+    console.log(`Score is ${score}`)
+    score++
+ } while (score<=10);
