74e29dfc7eccb888cb331ec5b73c7fdf8e753349
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-08 17:29:12 +0000

    for in loop concept by array in js

---
+++ 04_iteration/for_in_loop.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for(const key in myObject){
+    //console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+
+let programming = ["js", "rb", "cpp","swift"]
+for(const key in programming){
+    //console.log(`${key} ${programming[key]}`)
+}
+
+//Map
+const map =new Map()
+map.set('IN', 'India')
+map.set('US','United State')
+map.set('Fr', 'France')
+map.set('IN', 'India')
+
+//console.log(map)
+
+for (const [key, value] in map) {
+    console.log(key+':-'+value)
+}
