762d89db7565b954c65da03782b28c31c6ea93ba
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-09 17:00:43 +0000

    map functions basics in array

---
+++ 04_iteration/map_function.js
@@ -0,0 +1,11 @@
+//map is used to ferform myltiple operation at a time 
+
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumber.map( (num)=> num+10)
+
+const newNums = myNumber
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num >= 40)
+console.log(newNums)
