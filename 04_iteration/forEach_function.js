1a2c095dac45026be032dd9bd11fcfe648a8a159
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-09 15:17:47 +0000

    forEach function in array in js

---
+++ 04_iteration/forEach_function.js
@@ -0,0 +1,42 @@
+let coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (val){
+//     console.log(val)
+// })
+
+//forEach using araow function
+// coding.forEach( (item)=>{
+//     console.log(item)
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item,index,arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+})
